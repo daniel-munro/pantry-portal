685b3c82d6c64860897bf1adfc96dbde7c8ed2d1
Daniel Munro <danbmunro@gmail.com>
2024-11-28 22:17:46 -0600

    Add download tables and home page text

--- portal/static/js/download.js
+++ portal/static/js/download.js
@@ -2,7 +2,7 @@
 fetch('/api/tissues')
     .then(response => response.json())
     .then(tissues => {
-        ['rnaphenos-tissue-select', 'covariates-tissue-select'].forEach(selectId => {
+        ['rnaphenos-tissue-select', 'covariates-tissue-select', 'qtls-tissue-select', 'twasmodels-tissue-select'].forEach(selectId => {
             const tissueSelect = document.getElementById(selectId);
             tissueSelect.innerHTML = tissues
                 .map(tissue => `<option value="${tissue}">${tissue}</option>`)
@@ -14,22 +14,22 @@ fetch('/api/tissues')
     });
 
 const modalities = [
-    { file: 'alt_polyA', label: 'Alternative polyA' },
-    { file: 'alt_TSS', label: 'Alternative TSS' },
-    { file: 'expression', label: 'Expression' },
-    { file: 'isoforms', label: 'Isoform ratio' },
-    { file: 'splicing', label: 'Intron excision ratio' },
-    { file: 'stability', label: 'RNA stability' },
+    { id: 'alt_polyA', label: 'Alternative polyA' },
+    { id: 'alt_TSS', label: 'Alternative TSS' },
+    { id: 'expression', label: 'Expression' },
+    { id: 'isoforms', label: 'Isoform ratio' },
+    { id: 'splicing', label: 'Intron excision ratio' },
+    { id: 'stability', label: 'RNA stability' },
 ];
 
 const modalities_combined = [
-    { file: 'alt_polyA', label: 'Alternative polyA' },
-    { file: 'alt_TSS', label: 'Alternative TSS' },
-    { file: 'expression', label: 'Expression' },
-    { file: 'isoforms', label: 'Isoform ratio' },
-    { file: 'splicing', label: 'Intron excision ratio' },
-    { file: 'stability', label: 'RNA stability' },
-    { file: 'combined', label: 'Combined' }
+    { id: 'alt_polyA', label: 'Alternative polyA' },
+    { id: 'alt_TSS', label: 'Alternative TSS' },
+    { id: 'expression', label: 'Expression' },
+    { id: 'isoforms', label: 'Isoform ratio' },
+    { id: 'splicing', label: 'Intron excision ratio' },
+    { id: 'stability', label: 'RNA stability' },
+    { id: 'combined', label: 'Combined' }
 ];
 
 
@@ -40,7 +40,7 @@ document.getElementById('rnaphenos-tissue-select').addEventListener('change', fu
     tableBody.innerHTML = '';
 
     modalities.forEach(mod => {
-        const fileName = `${tissue}.${mod.file}.unnorm.bed.gz`;
+        const fileName = `${tissue}.${mod.id}.unnorm.bed.gz`;
         const row = `
             <tr>
                 <td><a href="/data/RNA_phenotypes/${fileName}" download>${fileName}</a></td>
@@ -58,14 +58,77 @@ document.getElementById('covariates-tissue-select').addEventListener('change', f
     tableBody.innerHTML = '';
 
     modalities_combined.forEach(mod => {
-        const fileName = `${tissue}.${mod.file}.covar.tsv`;
+        const fileName = `${tissue}.${mod.id}.covar.tsv`;
         const row = `
             <tr>
                 <td><a href="/data/covariates/${fileName}" download>${fileName}</a></td>
                 <td>${mod.label}</td>
+                <td>tensorQTL</td>
             </tr>
         `;
         tableBody.innerHTML += row;
+    });
+    modalities.forEach(mod => {
+        const fileName = `${tissue}.${mod.id}.covar.plink.tsv`;
+        const row = `
+            <tr>
+                <td><a href="/data/covariates/${fileName}" download>${fileName}</a></td>
+                <td>${mod.label}</td>
+                <td>PLINK</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+});
+
+// Build xQTLs table
+document.getElementById('qtls-tissue-select').addEventListener('change', function() {
+    const tissue = this.value;
+    const tableBody = document.getElementById('qtls-table-body');
+
+    modalities_combined.forEach(mod => {
+        const fileName1 = `${tissue}.${mod.id}.cis_qtl.txt.gz`;
+        const row1 = `
+            <tr>
+                <td><a href="/data/QTLs/${fileName1}" download>${fileName1}</a></td>
+                <td>${mod.label}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row1;
+        const fileName2 = `${tissue}.${mod.id}.cis_independent_qtl.txt.gz`;
+        const row2 = `
+            <tr>
+                <td><a href="/data/QTLs/${fileName2}" download>${fileName2}</a></td>
+                <td>${mod.label}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row2;
+    });
+});
+
+// Build TWAS models table
+document.getElementById('twasmodels-tissue-select').addEventListener('change', function() {
+    const tissue = this.value;
+    const tableBody = document.getElementById('twasmodels-table-body');
+    tableBody.innerHTML = '';
+
+    modalities.forEach(mod => {
+        const fileName1 = `${tissue}.${mod.id}.twas_weights.tar.bz2`;
+        const row1 = `
+            <tr>
+                <td><a href="/data/TWAS_weights/${fileName1}" download>${fileName1}</a></td>
+                <td>${mod.label}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row1;
+        const fileName2 = `${tissue}.${mod.id}.twas_weights.profile`;
+        const row2 = `
+            <tr>
+                <td><a href="/data/TWAS_weights/${fileName2}" download>${fileName2}</a></td>
+                <td>${mod.label}</td>
+            </tr>
+        `;
+        tableBody.innerHTML += row2;
     });
 });
 
